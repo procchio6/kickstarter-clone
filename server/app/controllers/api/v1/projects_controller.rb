55b2509ec39f62eebe64a575542cbcaa28e8e0fa
Pat Rocchio <procchio6@gmail.com>
2017-08-08 08:28:57 -0400

    Add project controller, routes, and serializer

---
+++ server/app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :update, :destroy]
+
+  def index
+    @projects = Project.all
+
+    render json: @projects
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      render json: @project, status: :created, location: @project
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project.destroy
+  end
+
+  private
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+
+    def project_params
+      params
+        .require(:project)
+        .permit(:name, :description, :funding_goal, :fund_by_date, :image, :user_id, :category_id)
+    end
+end
