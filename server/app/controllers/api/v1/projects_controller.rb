67cc8a84b8a358e32be481b44210df4a77510a15
Pat Rocchio <procchio6@gmail.com>
2017-08-08 09:02:46 -0400

    Remove bug in project creation

--- server/app/controllers/api/v1/projects_controller.rb
+++ server/app/controllers/api/v1/projects_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      render json: @project, status: :created, location: @project
+      render json: @project, status: :created
     else
       render json: @project.errors, status: :unprocessable_entity
     end
