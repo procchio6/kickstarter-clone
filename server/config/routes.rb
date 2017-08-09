6c7c0370eab3097e7c0eb2b3cbab4faa236cbc67
Pat Rocchio <procchio6@gmail.com>
2017-08-09 15:48:46 -0400

    Add server routes for login and users. Add initial user controller.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
+      post '/login', to: 'users#login'
+      resources :users, except: :destroy
+
       get '/projects/active', to: 'projects#active'
       get '/projects/inactive', to: 'projects#inactive'
       get '/projects/almost_funded', to: 'projects#almost_funded'
       get '/projects/almost_over', to: 'projects#almost_over'
       resources :projects
+
       resources :pledges
+
       get '/categories/:id/projects', to: 'categories#projects'
       resources :categories
     end
